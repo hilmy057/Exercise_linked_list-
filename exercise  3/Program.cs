6e3edd52ee370677a6af00dc7bc5b34e2231fe6b
hilmy057 <naufalh702@gmail.com>
2022-11-24 16:48:34 +0700

    exercise _3

---
+++ exercise  3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Double_Linked_List_exercise 
+{
+    //Each node consists of the information part and a lik to the next node
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+        public Node prev;
+    }
+    class DoubleLinkedList
+    {
+        Node start;
+        public DoubleLinkedList()
+        {
+            start = null;
+        }
+        //adds a node in the list
